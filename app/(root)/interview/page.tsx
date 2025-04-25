380aec6fa8587c355f20170532eeb2781e8b8629
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2025-04-25 17:33:02 -0500

    implement interview generation ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,11 @@
+import { Agent } from '@/components/Agent'
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+      <h3>Interview Generation</h3>
+      <Agent userName='You' userId='user1' type='generate' />
+    </>
+  )
+}
