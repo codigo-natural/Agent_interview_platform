e3b4c084e2faf3eb7cb6a7f456b5714abb5a7261
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2025-04-27 17:22:05 -0500

    show generated interviews

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,25 @@
 import { InterviewCard } from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
 import { dummyInterviews } from '@/constants'
+import {
+  getCurrentUser,
+  getInterviewsByUserId,
+  getLatestInterviews,
+} from '@/lib/actions/auth.action'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Home() {
+export default async function Home() {
+  const user = await getCurrentUser()
+
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterviewsByUserId(user?.id!),
+    await getLatestInterviews({ userId: user?.id! }),
+  ])
+
+  const hasPastInterviews = userInterviews?.length > 0
+  const hasUpcomingInterviews = latestInterviews?.length > 0
+
   return (
     <>
       <section className='card-cta'>
@@ -31,9 +46,15 @@ export default function Home() {
         <h2>Your Interviews</h2>
 
         <div className='interviews-section'>
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p className='text-lg'>
+              You haven&apos;t taken any interviews yet.
+            </p>
+          )}
         </div>
       </section>
 
@@ -41,9 +62,13 @@ export default function Home() {
         <h2>Take an Interview</h2>
 
         <div className='interviews-section'>
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
+          {hasUpcomingInterviews ? (
+            latestInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p className='text-lg'>There are no new interviews available.</p>
+          )}
         </div>
       </section>
     </>
